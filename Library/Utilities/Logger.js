2db89daac65cd805996ff17467bf72b5a396972f
Navy <navydotgif@gmail.com>
2019-12-28 16:29:00 +0200

    Class for logging

---
+++ Library/Utilities/Logger.js
@@ -0,0 +1,48 @@
+//const logfolder = require('../../Logs');
+const fs = require('fs');
+
+class Logger {
+
+    constructor() {
+
+        this.logbook;
+
+    }
+
+    async init() {
+
+        this.print('Starting logger.');
+        if(!fs.existsSync('../CorgiBot-Logs')) {
+            this.print('Log directory not found, creating new.')
+            await fs.mkdirSync('../CorgiBot-Logs');
+        }
+        this.logbook = fs.createWriteStream(`../CorgiBot-Logs/CorgiBot-${~~(Date.now()/1000)}.log`);
+        this.logbook.write(`====== CorgiBot Logbook - ${new Date().toUTCString()} ======\n\n`);
+        return this;
+
+    }
+
+    log(text) {
+
+        if(!this.logbook) return;
+        this.logbook.write(`${text}\n`);
+
+    }
+
+    print(text) {
+
+        this.log(text);
+        console.log(text);
+
+    }
+
+    error(text) {
+
+        this.log(`!! ${text} !!`);
+        console.error(`!! ${text} !!`);
+
+    }
+
+}
+
+module.exports = new Logger();
