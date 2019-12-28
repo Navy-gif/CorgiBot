9ed91d4149244a9d6aa6ad7ac4a15f1b0ce0deb6
Navy <navydotgif@gmail.com>
2019-12-28 18:27:59 +0200

    Unimplemented command default return

--- Library/Commands/Command.js
+++ Library/Commands/Command.js
@@ -3,13 +3,14 @@ class Command {
     constructor(data) {
 
         this.name = data.name;
-        
+        this.aliases = data.aliases || [];
+        this.group;
 
     }
 
     async call() {
 
-
+        return `This command (${this.name}) has yet to be defined.`;
 
     }
 
