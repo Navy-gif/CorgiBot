44dd5689703f78e652f559cf7ad8b343c4bbc579
Navy <navydotgif@gmail.com>
2019-12-29 15:06:22 +0200

    added a way to add new animal categories

--- Library/Commands/Utility/Contribute.js
+++ Library/Commands/Utility/Contribute.js
@@ -31,37 +31,59 @@ class Contribute extends Command {
 
             animal = args.shift();
 
-            if(!Object.keys(animals).includes(animal)) return new EmbeddedResponse('Invalid animal type or breed!').setTitle('**Error** ⚠️');
+            if(animal === 'category') {
 
-            if(!args[0] && message.attachments.size === 0) return new EmbeddedResponse('Provide a URL to an image!').setTitle('**Error** ⚠️');
+                if(!index.devs.includes(author.id)) return new EmbeddedResponse('Insufficient permissions!').setTitle('**Error** ⚠️');
+                if(args.length === 0) return new EmbeddedResponse('Provide a category name!').setTitle('**Error** ⚠️');
 
-            if(message.attachments.size > 0) {
+                let name = args.shift().toLowerCase();
+                index.animals[name] = [];
+                let error
+                let result = await db.updateOne('animals', { type: name }, { type: name, images: index.animals[name] }, true).catch(err => error = err);
 
-                message.attachments.forEach(att => {
-                    if(att.height > 0 && att.width > 0 && !animals[animal].includes(att.url)) {
-                        links.push(att.url);
-                        animals[animal].push(att.url);
-                    }
-                });
-                
-            }
+                if(error) {
+                    logger.error(error);
+                    return new EmbeddedResponse('Something went wrong with inserting to database.').setTitle('**Error** ⚠️');
+                } else {
+                    logger.print(`${author.tag} (${author.id}) created a new animal collection for ${name}. Result: ${JSON.stringify(result)}`);
+                    return new EmbeddedResponse(`Successfully created ${name} collection!`).setTitle('Success!');
+                }
 
-            for(let arg of args) for(let format of fileFormats) if(arg.toLowerCase().endsWith(format) && !animals[animal].includes(arg)) {
-                animals[animal].push(arg);
-                links.push(arg);
-            }
-
-            if(links.length === 0) return new EmbeddedResponse(`No recognized links nor attachments were found!\n\nMake sure your links and/or images are of the following formats:\n${fileFormats.join(', ')}`).setTitle('Error!');
-
-            let error;
-            let result = await db.push('animals', { type: animal }, { images: { $each: links } }).catch(err => error = err);
-            
-            if(error) {
-                logger.error(error);
-                return new EmbeddedResponse(`Failed to add ${links.length} images to the ${animal} collection.`).setTitle('Error!')
             } else {
-                logger.print(`${author.tag} (${author.id}) contributed by adding the following image(s) (${links.join(', ')}) to the ${animal} collection. Result: ${JSON.stringify(result)}`);
-                return new EmbeddedResponse(`Successfully added ${links.length} images to the ${animal} collection!`).setTitle('Success!')
+
+                if(!Object.keys(animals).includes(animal)) return new EmbeddedResponse('Invalid animal type or breed!').setTitle('**Error** ⚠️');
+
+                if(!args[0] && message.attachments.size === 0) return new EmbeddedResponse('Provide a URL to an image!').setTitle('**Error** ⚠️');
+    
+                if(message.attachments.size > 0) {
+    
+                    message.attachments.forEach(att => {
+                        if(att.height > 0 && att.width > 0 && !animals[animal].includes(att.url)) {
+                            links.push(att.url);
+                            animals[animal].push(att.url);
+                        }
+                    });
+                    
+                }
+    
+                for(let arg of args) for(let format of fileFormats) if(arg.toLowerCase().endsWith(format) && !animals[animal].includes(arg)) {
+                    animals[animal].push(arg);
+                    links.push(arg);
+                }
+    
+                if(links.length === 0) return new EmbeddedResponse(`No recognized links nor attachments were found!\n\nMake sure your links and/or images are of the following formats:\n${fileFormats.join(', ')}`).setTitle('Error!');
+    
+                let error;
+                let result = await db.push('animals', { type: animal }, { images: { $each: links } }).catch(err => error = err);
+                
+                if(error) {
+                    logger.error(error);
+                    return new EmbeddedResponse(`Failed to add ${links.length} image${links.length > 1 ? 's' : ''} to the ${animal} collection.`).setTitle('Error!');
+                } else {
+                    logger.print(`${author.tag} (${author.id}) contributed by adding the following image(s) (${links.join(', ')}) to the ${animal} collection. Result: ${JSON.stringify(result)}`);
+                    return new EmbeddedResponse(`Successfully added ${links.length} images to the ${animal} collection!`).setTitle('Success!');
+                }
+
             }
             
         }  
