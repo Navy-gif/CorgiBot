92089c7196ca8866de18d5dd6d774ee5bd613a9e
Navy <navydotgif@gmail.com>
2019-12-29 04:03:59 +0200

    contributors

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ module.exports = {
 	bot: null,
 	started: ~~(Date.now()/1000),
 	devs: ['132777808362471424'],
+	contributors: ['244897255260160001', '296320072379662336'],
 	util: {},
 	debug: true,
 	db: null,
-	database: null
+	database: null,
+	animals: {}
 }
 
 require('./Library/Utilities/Startup').init();
